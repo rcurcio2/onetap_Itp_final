0ec4e8da34b63eb00ffdaeed58f6a1bc42216336
Ryan Curcio <rcurcio@usc.edu>
2022-12-09 16:14:58 -0800

    device page final

--- src/pages/DevicePage.js
+++ src/pages/DevicePage.js
@@ -1,14 +1,17 @@
-import { Grid, Typography, Button } from '@mui/material';
-import { doc, onSnapshot} from '@firebase/firestore';
+import { Grid, Typography, Alert } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { doc, onSnapshot, getDoc, updateDoc} from '@firebase/firestore';
 import { Container, Box } from '@mui/system';
 import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
 import { capitalCase } from 'change-case';
-import Label from '../components/label';
+import { useSnackbar } from '../components/snackbar';
 
 import { useAuthContext } from '../auth/useAuthContext';
 import { DB } from '../auth/FirebaseContext';
+
+
 
 export default function DevicePour() {
   const { deviceName } = useParams();
@@ -20,17 +23,56 @@ export default function DevicePour() {
   const [cost, setCost] = useState('');
 
   const { user } = useAuthContext();
+  const { enqueueSnackbar } = useSnackbar();
+
+  async function getUserLock(uid) {
+    const userRef = doc(DB, 'users', uid);
+    const userDoc = await getDoc(userRef);
+    if (userDoc.exists()) {
+      return userDoc.data().displayName;
+    }
+    return null;
+  }
+
+  function handlePourClick() {
+    // Temporary until I finish the hardware of the device which will handle the pours
+    const deviceRef = doc(DB, 'machines', deviceName);
+    updateDoc(deviceRef, {
+      userLock: user.uid,
+    });
+
+    setTimeout(() => {
+      updateDoc(deviceRef, {
+        pouring: true,
+      }).then(() => {
+        enqueueSnackbar('Start pouring!');
+      });
+    }
+      , 2000);
+    
+    setTimeout(() => {
+      updateDoc(deviceRef, {
+        pouring: false,
+        userLock: 'open'
+      });
+    }
+      , 10000);
+    
+  }
 
   useEffect(() => {
     const deviceRef = doc(DB, "machines", deviceName);
-        const unsub = onSnapshot(deviceRef, (doc) => {
-            setOnline(doc.data().kegOnline);
-            setUserLock(doc.data().userLock);
-            setPouring(doc.data().pouring);
-            setName(doc.data().deviceName || '');
-            setOnTap(doc.data().onTap || '');
-            setCost(doc.data().costPerPint || '');
-        });
+    const unsub = onSnapshot(deviceRef, (doc) => {
+      getUserLock(doc.data().userLock).then((displayName) => {
+        setUserLock(displayName);
+      });    
+      setOnline(doc.data().kegOnline);
+      setUserLock();
+      setPouring(doc.data().pouring);
+      setName(doc.data().deviceName || '');
+      setOnTap(doc.data().onTap || '');
+      setCost(doc.data().costPerPint || '');
+    });
     return unsub;
   }, [deviceName]);
 
@@ -53,17 +95,41 @@ export default function DevicePour() {
           </Grid>
 
           <Grid item xs={12} align="center">
+
+            {
+              userLock === user.displayName && pouring ?
+                <Alert
+                  severity="success"
+                  sx={{ my: 3, display: pouring ? 'flex' : 'none' }}
+                >
+                  <Typography >Success! Start pouring now.</Typography>
+                </Alert>
+                : 
+                <Alert
+                  severity="info"
+                  sx={{ my: 3, display: pouring ? 'flex' : 'none' }}
+                  
+                >
+                  <Typography >{userLock} is currently pouring. Please wait.</Typography>
+                </Alert>
+            }
+           
             <Container maxWidth="sm">
-              <Button
+              <LoadingButton
                 variant="contained"
                 color="primary"
                 size='large'
                 fullWidth
-                disabled= { !online || pouring }
+                disabled={!online || pouring || !!userLock}
+                loading={ userLock === user.displayName && !pouring }
+                onClick={() => {
+                  handlePourClick();
+                }}
               >
                 { !online ? 'Device Offline' : 'Pour'}
-              </Button>
+              </LoadingButton>
             </Container>
+
           </Grid>
 
           <Grid item xs={12} align="center">
